ddeb1e4c0b2037c83d653d04c4908721e8a90879
Gabor Sornyei <info@sornyei.com>
2020-12-18 16:37:50 +0100

    Initial Commit

---
+++ Szentiras/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Szentiras
+//
+//  Created by Gabor Sornyei on 2020. 12. 18..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Szentiras/SzentirasApp.swift
@@ -0,0 +1,17 @@
+//
+//  SzentirasApp.swift
+//  Szentiras
+//
+//  Created by Gabor Sornyei on 2020. 12. 18..
+//
+
+import SwiftUI
+
+@main
+struct SzentirasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
